3f0fb0bdc7be14d131a5cfdfa290ace5b77c3b62
MegaDraconius <ivanvclinetor@gmail.com>
2024-04-21 22:12:51 +0200

    Add styles to the component soundwave-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { SoundwaveSectionComponent } from './soundwave-section/soundwave-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SoundwaveSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
