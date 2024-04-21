e0c34b349f2023fd235a349224e7a99381ea658f
MegaDraconius <ivanvclinetor@gmail.com>
2024-04-21 20:26:08 +0200

    create the main page from soundwave page

---
+++ src/app/soundwave-section/soundwave-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soundwave-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './soundwave-section.component.html',
+  styleUrl: './soundwave-section.component.scss'
+})
+export class SoundwaveSectionComponent {
+
+}
