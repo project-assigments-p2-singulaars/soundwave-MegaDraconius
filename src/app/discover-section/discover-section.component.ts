6de72d9a0429b72f412f9ba5154b59c6256bd20f
MegaDraconius <ivanvclinetor@gmail.com>
2024-04-22 16:00:20 +0200

    Add routes to the soundwave project and correct styles

--- src/app/discover-section/discover-section.component.ts
+++ src/app/discover-section/discover-section.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-discover-section',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './discover-section.component.html',
-  styleUrl: './discover-section.component.scss'
+  styleUrl: './discover-section.component.scss',
 })
-export class DiscoverSectionComponent {
-
-}
+export class DiscoverSectionComponent {}
--- src/app/soundwave-section/soundwave-section.component.ts
+++ src/app/soundwave-section/soundwave-section.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-soundwave-section',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, RouterLink],
   templateUrl: './soundwave-section.component.html',
-  styleUrl: './soundwave-section.component.scss'
+  styleUrl: './soundwave-section.component.scss',
 })
-export class SoundwaveSectionComponent {
-
-}
+export class SoundwaveSectionComponent {}
